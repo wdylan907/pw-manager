754c746e0c2e02370087f7ba5ff0af080901231b
Dylan White <wdylan907@gmail.com>
2021-12-23 12:40:58 -0500

    fix cors import

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const session = require('express-session')
+const cors = require('cors')
 const config = require('./config')
 const router = require('./controllers/router')
 
