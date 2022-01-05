ce067791e1984cb71e7db7510879510e260502e2
Dylan White <wdylan907@gmail.com>
2022-01-05 13:13:37 -0500

    add accordion to dashboard

--- client/src/views/dashboard.js
+++ client/src/views/dashboard.js
@@ -1,6 +1,13 @@
 import { useState, useEffect } from 'react'
 import Entry from './components/Entry'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Accordion from 'react-bootstrap/Accordion'
+
 const Dashboard = props => {
   const { serverUrl, axios } = props.config
   const [userData, setUserData] = useState('')
@@ -16,18 +23,33 @@ const Dashboard = props => {
 
   if (userData) {
     return (
-      <>
-        <h3>dashboard</h3>
-        {userData.username}
-        <br />
-        <br />
-        {userData.vault.map(obj => {
-          return <Entry key={obj.id} label={obj.label} />
-        })}
-        <br />
-        <br />
-        <button onClick={props.onClick}>logout</button>
-      </>
+      <div>
+        <Row>
+          <Container className='pt-5 pb-2 col-6 align-self-center'>
+            <h1>Dashboard</h1>
+          </Container>
+        </Row>
+        <Row className='pt-4 pb-3'>
+          <Col sm={3} xs={0}></Col>
+          <Col sm={6} xs={12}>
+            <Container
+              className='pt-3 pb-3'
+              style={{ border: '5px solid #cecece' }}
+            >
+              <Accordion>
+                {userData.vault.map(entry => {
+                  return (
+                    <Accordion.Item key={entry.id} eventKey={entry.id}>
+                      <Accordion.Header>{entry.label}</Accordion.Header>
+                      <Accordion.Body>body</Accordion.Body>
+                    </Accordion.Item>
+                  )
+                })}
+              </Accordion>
+            </Container>
+          </Col>
+        </Row>
+      </div>
     )
   } else return null
 }
