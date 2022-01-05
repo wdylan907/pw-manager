b55e7e93d1826c9f6c7abcf856b56be0fe225ec8
Dylan White <wdylan907@gmail.com>
2022-01-05 12:11:26 -0500

    redo login w/ bootstrap

--- client/src/views/login.js
+++ client/src/views/login.js
@@ -6,24 +6,6 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 const Login = props => {
-  // return (
-  //   <>
-  //     <h3>login</h3>
-  //     <form onSubmit={props.onSubmit}>
-  //       username:
-  //       <input name='username' />
-  //       <br />
-  //       password:
-  //       <input name='password' type='password' />
-  //       <br />
-  //       <button type='submit'>submit</button>
-  //     </form>
-  //     <br />
-  //     <br />
-  //     <button onClick={props.link}>register</button>
-  //   </>
-  // )
-
   return (
     <div>
       <Row>
