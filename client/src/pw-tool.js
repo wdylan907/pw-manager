775a0cf895d79a46af89b107987b7c928f69c4a2
Dylan White <wdylan907@gmail.com>
2022-01-10 13:06:49 -0500

    refactor pw gen

--- client/src/pw-tool.js
+++ client/src/pw-tool.js
@@ -1,7 +1,7 @@
 const passwordGenerator = () => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
-  let string
+  let string = ''
   for (let i = 0; i < 24; i++) {
     string += chars.charAt(Math.floor(Math.random() * chars.length))
   }
