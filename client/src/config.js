ed2c3ca8e148276d0794f386e75051b5ec93979e
Dylan White <wdylan907@gmail.com>
2022-01-24 17:49:59 -0500

    fix serverUrl in client config

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 axios.defaults.withCredentials = true
 
 //const serverUrl = 'http://localhost:5000'
-const serverUrl = 'http://blooming-anchorage-99953.heroku.com'
+const serverUrl = 'http://blooming-anchorage-99953.herokuapp.com'
 
 const config = {
   serverUrl: serverUrl,
